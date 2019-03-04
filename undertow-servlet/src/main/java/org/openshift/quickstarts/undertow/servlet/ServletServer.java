c9debccb6d1e29d29d0e2923498947f3a0e3a50c
mrcool76 <48221403+mrcool76@users.noreply.github.com>
2019-03-04 21:36:53 +0000

    Update ServletServer.java

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "TETS SERVER")
+                                    .addInitParam("message", "MAX IS BAD AT CODING")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
